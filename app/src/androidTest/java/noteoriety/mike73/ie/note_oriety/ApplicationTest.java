c946c75eb852eedade7a1ffd580cc784805ba1c6
= <michaelreid73@gmail.com>
2016-08-12 19:08:26 +0100

    initial commit. nothing to see here.

---
+++ app/src/androidTest/java/noteoriety/mike73/ie/note_oriety/ApplicationTest.java
@@ -0,0 +1,13 @@
+package noteoriety.mike73.ie.note_oriety;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
